6c290887c107070736703d2051eb7a4d2c1f6821
luy <luyuanjiejie@qq.com>
2026-07-25 23:21:39 +0800

    7.整数反转

---
+++ onebyone/7.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        let custom =  2147483648
+        var temp = x;
+        temp = abs(temp)
+        var res = 0;
+        while(temp > 0 ) {
+            var tail = temp % 10
+            res = res * 10 + tail
+            temp /= 10;
+
+        }
+        res *= x < 0 ? -1 : 1;
+        return -1*custom < res && custom - 1 > res ? res : 0
+    }
+}
