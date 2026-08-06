2ab28b2a48ef5c3bea493e980cec3964383de041
yuan <luyuanjiejie@qq.com>
2026-08-06 23:58:53 +0800

    17.电话号码的字母组合

---
+++ onebyone/17.swift
@@ -0,0 +1,29 @@
+class Solution {
+    func letterCombinations(_ digits: String) -> [String] {
+        var m = [
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        ]
+        var digits = digits.map({ (i: Character) -> String in return m[String(i)]! })
+
+        var arr = [String]()
+        func loop(_ s: String, _ h: Int) {
+            if digits.count <= h {
+                if s != "" { arr.append(s) }
+                return
+            }
+            for i in digits[h] {
+                loop(s + String(i), h + 1)
+            }
+        }
+
+        loop("", 0)
+        return arr
+    }
+}
